ae7c4592fab2c49ed59918ba746e5ded81e4de29
aMytho <58316242+aMytho@users.noreply.github.com>
2024-09-30 19:04:27 -0500

    Fix down migrations

--- migrations/2024-07-04-204028_create_tables/down.sql
+++ migrations/2024-07-04-204028_create_tables/down.sql
@@ -1,7 +1,7 @@
 -- This file should undo anything in `up.sql`
 PRAGMA foreign_keys = ON;
-DROP TABLE tracks;
-DROP TABLE artists;
-DROP TABLE albums;
-DROP TABLE playlists;
 DROP TABLE playlist_tracks;
+DROP TABLE playlists;
+DROP TABLE albums;
+DROP TABLE artists;
+DROP TABLE tracks;
