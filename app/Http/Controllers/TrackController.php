2da3188436a22a7bd68c4558da5d16f7eef1503d
aMytho <58316242+aMytho@users.noreply.github.com>
2024-12-09 21:59:06 -0600

    Allow web scan, start search rewrite

--- app/Http/Controllers/TrackController.php
+++ app/Http/Controllers/TrackController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TrackSearchRequest;
+use App\Jobs\ScanMusic;
 use App\Models\Track;
 use Kiwilan\Audio\Audio;
 
@@ -17,13 +19,24 @@ class TrackController extends Controller
     return response()->make()->status(400);
   }
 
-  public function search()
+  public function search(TrackSearchRequest $request)
   {
-    return response()->json(Track::all());
+    $tracks = Track::with('artist')
+        ->where('title', 'like', '%' . ($request->validated('track') ?? '') . '%')
+        ->skip($request->validated('track_offset') ?? 0)
+        ->take(25)
+        ->get();
+
+    return response()->json($tracks);
   }
 
   public function stream(Track $track)
   {
     return response()->file($track->location);
+  }
+
+  public function scan()
+  {
+    ScanMusic::dispatch();
   }
 }
