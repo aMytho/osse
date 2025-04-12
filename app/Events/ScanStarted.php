f755061b3ef5a5f008c37690a65f71d1a80fc1b3
aMytho <58316242+aMytho@users.noreply.github.com>
2025-04-12 15:11:32 -0500

    Switch to public channels since we use redis

--- app/Events/ScanStarted.php
+++ app/Events/ScanStarted.php
@@ -2,8 +2,8 @@
 
 namespace App\Events;
 
+use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
@@ -27,7 +27,7 @@ class ScanStarted implements ShouldBroadcastNow
     public function broadcastOn(): array
     {
         return [
-            new PrivateChannel('scan'),
+            new Channel('scan'),
         ];
     }
 }
