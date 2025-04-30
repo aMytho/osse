4751c3b8295e4c8dec9a056116236c0f5e44fde1
aMytho <58316242+aMytho@users.noreply.github.com>
2025-04-29 21:17:11 -0500

    Add php ide helper

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -7,6 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property int|null $year
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Artist> $artists
+ * @property-read int|null $artists_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Track> $tracks
+ * @property-read int|null $tracks_count
+ * @method static \Database\Factories\AlbumFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Album newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Album newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Album query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Album whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Album whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Album whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Album whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Album whereYear($value)
+ * @mixin \Eloquent
+ */
 class Album extends Model
 {
     /** @use HasFactory<\Database\Factories\AlbumFactory> */
